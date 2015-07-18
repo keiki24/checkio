3af48f4de104659ebbd9af6e967fd33964d94c7d
keiki24 <kabotyanopurin@gmail.com>
2015-07-18 11:10:37 +0900

    add new solutions

---
+++ Home/moore_neighbourhood.py
@@ -0,0 +1,32 @@
+def count_neighbours(grid, row, col):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if row+j >= 0 and col+i >= 0:
+                try:
+                    if grid[row+j][col+i] == 1:
+                        count += 1
+                except IndexError:
+                    pass
+    if grid[row][col] == 1:
+        count -= 1
+    return count
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
